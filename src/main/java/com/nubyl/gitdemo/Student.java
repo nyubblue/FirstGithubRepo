619b95bb1461d77b2e40644d0bc710ff2099262c
Buyn Le <thanhbuynitu@gmail.com>
2019-07-13 16:24:59 +0700

    first commit

---
+++ src/main/java/com/nubyl/gitdemo/Student.java
@@ -0,0 +1,25 @@
+package com.nubyl.gitdemo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * 
+ */
+
+/**
+ *
+ * @author buynl
+ */
+public class Student {
+    private int id;
+    private String name;
+    private String address;
+
+    public Student(int id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+    
+    
+}
